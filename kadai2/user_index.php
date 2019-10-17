2e906aa05c25e7d228faefad3ce1d8d9c16f9510
Shimizuke <kei.s61.0908@gmail.com>
2019-10-17 21:35:20 +0900

    second

--- kadai2/user_index.php
+++ kadai2/user_index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ユーザー登録</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/css.css">
     <style>
         div {
             padding: 10px;
@@ -43,24 +44,42 @@
         </div>
         <div class="form-group">
             <label for="lid">ログインID</label>
-            <input type="text" class="form-control" id="lid" name="lid" rows="3">
+            <input type="text" class="form-control" id="lid" name="lid">
         </div>
         <div class="form-group">
             <label for="lpw">ログインパスワード</label>
-            <input type="text" class="form-control" id="lpw" name="lpw" rows="3">
+            <input type="text" class="form-control" id="lpw" name="lpw">
         </div>
         <div class="form-group">
-            <label for="kanri_flg">一般:0,管理者:1</label>
-            <input type="text" class="form-control" id="kanri_flg" name="kanri_flg" rows="3">
+            <label for="kanri_flg">一般or管理者</label>
+            <!-- <input type="text" class="form-control" id="kanri_flg" name="kanri_flg"> -->
+            <input type="hidden" class="form-group" id="kanri_flg" name="kanri_flg" value="$shift">
+            <p>一般</p>
+            <input type="radio" class="form-group" id="kanri_flg" name="kanri_flg" value=0 $checked0 />
+            <p>管理者</p>
+            <input type="radio" class="form-group" id="kanri_flg" name="kanri_flg" value=1 $checked1 />
         </div>
         <div class="form-group">
-            <label for="life_flg">アクティブ:0,非アクティブ:1</label>
-            <input type="text" class="form-control" id="life_flg" name="life_flg" rows="3">
+            <label for="life_flg">アクティブor非アクティブ</label>
+            <!-- <input type="text" class="form-control" id="life_flg" name="life_flg"> -->
+            <input type="hidden" class="form-group" id="life_flg" name="life_flg" value="$shift">
+            <p>アクティブ</p>
+            <input type="radio" class="form-group" id="life_flg" name="life_flg" value=0 $checked0 />
+            <p>非アクティブ</p>
+            <input type="radio" class="form-group" id="life_flg" name="life_flg" value=1 $checked1 />
         </div>
         <button type="submit" class="btn btn-primary">承認</button>
         </div>
+
     </form>
 
 </body>
+<?php
+$shift = 0;
+$checked0 = ($shift) ? "" : "checked";
+$checked1 = ($shift) ? "checked" : "";
+echo <<< EOT
+EOT;
+?>
 
 </html>
